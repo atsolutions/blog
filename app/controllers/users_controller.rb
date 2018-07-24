2b612ac0691f1668e187c0b1e097c365411fcc7c
Janis Klavins <janis@atsolutions.lv>
2018-07-24 20:55:12 +0300

    more user authorizations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ def create
     @user = User.new  (user_params)
         if @user.save
             flash[:success] = "You have succesfully signed up" 
+            session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             render :new
